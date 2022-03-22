72b56402ff2bfdcf3d2c223c68b6d755e2c1d009
Luna Ribeiro <lunasantos@alunos.utfpr.edu.br>
2022-03-22 15:16:25 -0300

    Introdução ao React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://nerdhits.com.br/wp-content/uploads/2021/09/shippuden-significado-1200x720.jpg" alt="imagem tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
