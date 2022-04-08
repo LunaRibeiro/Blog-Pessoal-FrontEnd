fe5d744b57245b8de160c93b4c308583aebd7c7d
Luna Ribeiro <lunasantos@alunos.utfpr.edu.br>
2022-04-08 16:14:08 -0300

    Deletar, Modal e Cadastro em Postagens

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import './Footer.css'
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub'
 import {Typography, Box, Grid } from '@material-ui/core';
+import './Footer.css';
 
 function Footer() {
     return (
@@ -14,17 +15,25 @@ function Footer() {
                             <Typography variant="h5" align="center" gutterBottom className='textos'>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/LunaRibeiro" target="_blank">
-                                <GitHubIcon className="redes" />
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <FacebookIcon className='redes' />
                             </a>
-                            <a href="https://www.linkedin.com/in/luna-ribeiro-alves/" target="_blank">
-                                <LinkedInIcon className="redes" />
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
+                                <InstagramIcon className='redes' />
+                            </a>
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                <LinkedInIcon className='redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box >
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright: Luna Ribeiro</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='textos' >© 2020 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org">
+                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">brasil.generation.org</Typography>
+                            </a>
                         </Box>
                     </Box>
                 </Grid>
