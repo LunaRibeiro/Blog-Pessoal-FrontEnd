1f00f78cf2eb837804ab2ca1c967eb2f8440a35e
Luna Ribeiro <lunasantos@alunos.utfpr.edu.br>
2022-04-13 17:03:17 -0300

    Revert "Alteração no ListaTema.tsx"
    This reverts commit b713b10acd6de0315ed7a5c59147e8bcdc465408.

--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,55 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import Tema from '../../../models/Tema';
 import './ListaTema.css';
-import {useHistory} from 'react-router-dom';
-import { busca } from '../../../services/Service';
-import { useSelector } from 'react-redux';
-import { TokenState } from '../../../store/tokens/tokensReducer';
-import { toast } from 'react-toastify';
 
 function ListaTema() {
-  const [temas, setTemas] = useState<Tema[]>([])
-  let history = useHistory();
-  const token = useSelector<TokenState, TokenState["tokens"]>(
-    (state) => state.tokens
-  );
-
-  useEffect(()=>{
-    if(token == ''){
-      toast.error('Você precisa estar logado', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: "colored",
-        progress: undefined,
-        });
-      history.push("/login")
-    }
-  }, [token])
-
-
-  async function getTema(){
-    await busca("/tema", setTemas, {
-      headers: {
-        'Authorization': token
-      }
-    })
-  }
-
-
-  useEffect(()=>{
-    getTema()
-  }, [temas.length])
 
   return (
     <>
-    {
-      temas.map(tema =>(
       <Box m={2} >
         <Card variant="outlined">
           <CardContent>
@@ -57,20 +14,20 @@ function ListaTema() {
               Tema
             </Typography>
             <Typography variant="h5" component="h2">
-             {tema.descricao}
+              Minha descrição
             </Typography>
           </CardContent>
           <CardActions>
             <Box display="flex" justifyContent="center" mb={1.5} >
 
-              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+              <Link to="" className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
                   </Button>
                 </Box>
               </Link>
-              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+              <Link to="" className="text-decorator-none">
                 <Box mx={1}>
                   <Button variant="contained" size='small' color="secondary">
                     deletar
@@ -81,8 +38,6 @@ function ListaTema() {
           </CardActions>
         </Card>
       </Box>
-      ))
-      }
     </>
   );
 }
